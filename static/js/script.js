ddd2c9396a8148be34e51106ee31a712c9d62bbd
Dan Stoakes <dan.stoakes8@gmail.com>
2024-01-13 12:44:05 +0000

    Add some slick desktop transitions

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
     const Navigation = (() => {
-        function _setupHamburger() {
+        function _setupHamburger () {
             const hamburger = document.querySelector(".hamburger-menu");
             const navUL = document.querySelector("nav ul");
         
-            hamburger.addEventListener("click", function() {
+            hamburger.addEventListener("click", () => {
                 navUL.classList.toggle("active");
             });
         }
 
+        function _setupSearch () {
+            const searchButton = document.querySelector("nav ul .search-icon");
+
+            searchButton.addEventListener("click", () => {
+                const dropdownNav = document.querySelector(".nav-dropdown");
+                const mainNav = document.querySelector("nav");
+
+                dropdownNav.classList.toggle("nav-dropdown-hidden");
+                mainNav.classList.toggle("dropdown-extended");
+            });
+        }
+
         return {
-            setupHamburger: _setupHamburger
+            setupHamburger: _setupHamburger,
+            setupSearch: _setupSearch
         };
     })();
 
     Navigation.setupHamburger();
+    Navigation.setupSearch();
 });
